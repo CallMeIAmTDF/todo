9f90676a4ac9810ee7a03c0d75591f59b5600f95
CallMeIAmTDF <131275170+CallMeIAmTDF@users.noreply.github.com>
2025-04-09 12:45:53 +0700

    Update todoRoutes.js

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todoController');
 
+router.get('/hello', () => {return "Hello";})
 router.get('/', todoController.getAllTodos);
 router.get('/:id', todoController.getTodoById);
 router.post('/', todoController.createTodo);
